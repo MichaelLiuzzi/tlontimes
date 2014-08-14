274552c109fa567ff2a4e092851d96a9624de9f1
michaelliuzzi <michael.r.liuzzi@gmail.com>
2014-08-14 15:07:33 -0400

    Persistence Works. articletemplate_edit_route.js works as it should. Hallelujah.

--- app/scripts/controllers/articletemplate_controller.js
+++ app/scripts/controllers/articletemplate_controller.js
@@ -1,8 +1,59 @@
 Tlontimesyo.ArticleTemplateController = Ember.ObjectController.extend({
 	titleEdit: false,
 	articleTitleEditClick: false,
-	newTitle:"Your Article Title",
+	newTitle:"A Most Grave Incident",
+	authorEdit: false,
+	articleAuthorEditClick: false,
+	newAuthor:"Dr. WHO?",
+	timeEdit: false,
+	newTime: "THE PAST, THE FUTURE",
+	articleTimeEditClick: false,
 	newId: null,
+
+	createRecord: function() {
+
+			this.newId = Math.floor((Math.random() * 10000));
+
+			var newArticle = this.store.createRecord('article', {
+				id: this.newId,
+			});
+
+
+			newArticle.save();
+
+
+		},
+
+	updateInterface: function(key) {
+
+		if (key === "articleTitle")
+		{
+
+		this.set('articleTitleEditClick', false); 
+
+		this.set('titleEdit', false);
+		}
+
+		if (key === "Author")
+		{
+			this.set('articleAuthorEditClick', false);
+
+			this.set('authorEdit', false);
+		}
+
+		if( key === "time") 
+		{
+			this.set('articleTimeEditClick', false);
+			this.set('timeEdit', false);
+
+
+		}
+
+
+	},
+
+
+
 
 	actions: {
 
@@ -12,6 +63,53 @@ Tlontimesyo.ArticleTemplateController = Ember.ObjectController.extend({
 			this.set('articleTitleEditClick', true);
 
 		},
+
+		authorEditMethod: function(){
+			this.set('articleAuthorEditClick', true);
+			this.set('authorEdit', true);
+		},
+
+		timeEditMethod: function(){
+			this.set('articleTimeEditClick', true);
+			this.set('timeEdit', true);
+		},
+
+		persist: function(key, keyValue) {
+			if(this.newId === null) 
+			{
+				this.createRecord();
+			}
+
+			//RETRIEVE AND UPDATE RECORD
+
+			//save controller context 
+			var controller = this; 
+
+			//use closure to retain controller context while we wait for promise to resolve 
+			(function(controller, key, keyValue){
+			
+			
+			//returns a promise object
+			//value = the fulfilled promise (i.e. a record)
+			var retrievedArticle = controller.store.find('article', controller.newId);
+			retrievedArticle.then(
+				function(value)
+				{
+					console.log("worked");
+					value.set(key, keyValue);
+					//IS THIS SAVING PROPERLY? Some sort of glitch where the saved article won't render properly
+					value.save();
+				
+				}, function(){console.log("didn't work")});
+			})(controller, key, keyValue);
+
+
+			//UPDATE INTERFACE
+			this.updateInterface(key); 
+		},
+
+
+
 
 		persistTitle: function(){
 
@@ -55,10 +153,9 @@ Tlontimesyo.ArticleTemplateController = Ember.ObjectController.extend({
 					
 					}, function(){console.log("didn't work")});
 
-			})(controller);
+				})(controller);
 				
-				/*retrievedArticle.set('articleTitle', this.newTitle);
-				retrievedArticle.save();*/
+				
 			}
 
 
--- app/scripts/routes/articletemplate_edit_route.js
+++ app/scripts/routes/articletemplate_edit_route.js
@@ -1,7 +1,18 @@
 Tlontimesyo.ArticleTemplateEditRoute = Ember.Route.extend({
 	model: function(article){ 
 		//EMBER WILL EXTRACT DYNAMIC SEGMENT I.E /: from url and pass to model hook.
-		return (Ember.$.getJSON('articles/' + article.id));
+		//return Ember.$.getJSON('articles/' + article.id);
+		var id = article.article_id;
+		return this.store.find('article', id);
+		
+		/*newArticle.then(function(value){
+			return value; 
 
-	}
+		}); 
+		*/
+    	
+
+    	//return ('/#/articles/'+id);
+  }
+
 });
